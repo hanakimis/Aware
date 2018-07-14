2f5800aeeefce792d4f11187aee7a914f72f4371
Hana Kim <hanakimis@gmail.com>
2018-07-13 20:40:08 -0700

    first commit, getting simple phototaking to work

---
+++ Aware/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  Aware (Mono no aware)
+//
+//  Created by Hana Kim on 7/12/18.
+//  Copyright © 2018 Hana Kim. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var previewView: UIView!
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var capturePhotoOutput: AVCapturePhotoOutput?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+            
+            capturePhotoOutput = AVCapturePhotoOutput()
+            capturePhotoOutput?.isHighResolutionCaptureEnabled = true
+            captureSession?.addOutput(capturePhotoOutput!)
+            
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            previewView.layer.addSublayer(videoPreviewLayer!)
+            captureSession?.startRunning()
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        guard let capturePhotoOutput = self.capturePhotoOutput else { return }
+        
+        let photoSettings = AVCapturePhotoSettings()
+        photoSettings.isAutoStillImageStabilizationEnabled = true
+        photoSettings.isHighResolutionPhotoEnabled = true
+        photoSettings.flashMode = .auto
+        
+        capturePhotoOutput.capturePhoto(with: photoSettings, delegate: self)
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension ViewController : AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+
+        let imageData = photo.fileDataRepresentation()
+        let captureImage = UIImage.init(data: imageData!, scale: 1.0)
+       
+        if let image = captureImage {
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        }
+    }
+    
+}
+
+
